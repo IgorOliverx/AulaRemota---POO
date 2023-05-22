22c021f39841b47be66493633fd319466aee7eaf
IgorOliverx <123770573+IgorOliverx@users.noreply.github.com>
2023-05-22 23:00:33 +0000

    22/05/2023

---
+++ ListraExtraForWhile/App.java
@@ -0,0 +1,9 @@
+package ListraExtraForWhile;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Exercicios obj = new Exercicios();
+        obj.exercicio2();
+    }
+}
---
+++ ListraExtraForWhile/Exercicios.java
@@ -0,0 +1,113 @@
+package ListraExtraForWhile;
+
+import java.util.Scanner;
+
+public class Exercicios {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    
+    public void exercicio1() {
+
+        System.out.println("Digite o seu nome de usúario:");
+        String nomeUsuario = sc.nextLine();
+        String senha;
+
+        for (;;) {
+            System.out.println("Digite sua senha:");
+            senha = sc.nextLine();
+
+            if (!senha.equals(nomeUsuario)) {
+                break;
+            }
+
+            System.out.println("Por favor, insira uma senha diferente da do seu usúario. Tente novamente");
+
+            
+        }
+
+        System.out.println("Nome de usuário:" + nomeUsuario);
+        System.out.println("Senha de usuário: "+ senha );
+
+    }
+
+    public void exercicio2() {
+
+        System.out.println("Vou ler e validar algumas de suas informações, vamos começar.");
+
+        String nome;
+        int idade;
+        int salario;
+        String sexo;
+        String estadoCivil;
+
+        System.out.println("Qual o Seu nome?");
+
+        for (;;) {
+
+            nome = sc.nextLine();
+
+            if(nome.length()>3) {
+                break;
+            } else {
+                System.out.println("Inválido, tente novamente!");
+            }
+            
+        }
+
+        for(;;) {
+            System.out.println("Qual a sua idade?");
+
+            idade = sc.nextInt();
+
+            if(idade >= 0 && idade <= 150) {
+                break;
+            } else {
+                System.out.println("Inválido, tente novamente!");
+            }
+        }
+
+        for(;;) {
+            System.out.println("Qual o seu salário?");
+
+            salario = sc.nextInt();
+
+            if(salario > 0) {
+
+                break;
+
+            } else {
+                System.out.println("Inválido, tente novamente!");
+            }
+        }
+
+        for(;;) {
+            System.out.println("Qual o seu sexo? f - feminino e m - masculino");
+
+            sexo = sc.nextLine();
+            if (sexo == "f" || sexo == "m") {
+                break;
+            } else {
+                System.out.println("Inválido, tente novamente!");
+            }
+        }
+        for(;;) {
+            System.out.println("Qual o seu estado civil? s - solteiro; c - casado; v - vuivo; d - ");
+
+            estadoCivil = sc.nextLine();
+            if (estadoCivil == "c" || estadoCivil == "v" || estadoCivil == "s" || estadoCivil == "d") {
+                break;
+            } else {
+                System.out.println("Inválido, tente novamente!");
+            }
+        }
+
+        System.out.println("Informações válidas:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Estado Civil: " + estadoCivil);
+        
+    }
+}
